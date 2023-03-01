03635e158e9dc7f32fff6972a2d71f6dab6b4086
liningxu <414397641@qq.com>
2023-03-01 21:15:09 +0800

    first commit

---
+++ packages/cli/bin/cli.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import greeting1 from '@lerna-basic-usage/a'
+import greeting2 from '@lerna-basic-usage/b'
+
+console.log(greeting1())
+console.log(greeting2())
+console.log('Hello from module CLI')
